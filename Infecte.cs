e03d12e099994c472625f0405a3dbf3779116823
Moumni Mohammed <64795535+Moumni-Mohammed@users.noreply.github.com>
2020-12-16 15:42:03 +0100

    Add files via upload

---
+++ Infecte.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Infecte : Citoyen
+{
+   private int numInfecte;
+   private bool confinerChezSoi;
+   private bool confinrAhopitale;
+   private bool vaccine;
+   private bool guerie;
+   private bool decede;
+   private bool testPCR2;
+   private bool seGraver;
+
+ 
+   public bool Reanimation()
+   {
+      throw new NotImplementedException();
+   }
+
+}
---
+++ NonInfecte.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class NonInfecte : Citoyen
+{
+   private bool vaccine;
+   private bool testPCR2;
+
+}

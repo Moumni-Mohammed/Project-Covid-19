d27e4f2b30ddaf3568a9a0f9f9475152be68fde3
Moumni Mohammed <64795535+Moumni-Mohammed@users.noreply.github.com>
2021-01-18 22:46:46 +0100

    Update and rename Citoyen.cs to Patient.cs

---
+++ Patient.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Patient
+{
+   private string nom;
+   private string prenom;
+   private int cin;
+   private int age;
+   private string adresse;
+   private int P_id ;
+
+}

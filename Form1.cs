9d4c98af7c0393a20fd8feb71555f5bb712a1a47
Moumni Mohammed <64795535+Moumni-Mohammed@users.noreply.github.com>
2021-01-18 22:43:04 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -19,20 +19,20 @@ namespace Corona
         }
 
 
-        SqlConnection cnx = new SqlConnection(@"Data Source=DESKTOP-S86SJHL;Initial Catalog=Covid;Integrated Security=True");
+        SqlConnection cnx = new SqlConnection(@"Data Source=DESKTOP-S86SJHL;Initial Catalog=Corona;Integrated Security=True");
 
         public int IdCitoyen;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            GetCitoyenRecord();
+            GetPatientRecord();
 
         }
 
-        private void GetCitoyenRecord()
+        private void GetPatientRecord()
         {
 
-            SqlCommand cmd = new SqlCommand("Select * from Citoyen", cnx);
+            SqlCommand cmd = new SqlCommand("Select * from AddPatient", cnx);
             DataTable dt = new DataTable();
 
             cnx.Open();
@@ -49,22 +49,15 @@ namespace Corona
         {
 
 
-            SqlCommand cmd = new SqlCommand("INSERT INTO Citoyen VALUES (@cin , @nom ,@prenom ,@age ,@location ,@aMaladieChronique ,@aSymptomesGrave)", cnx);
+            SqlCommand cmd = new SqlCommand("INSERT INTO AddPatient VALUES (@First_name , @Last_name ,@cin ,@age ,@adresse ,@pid)", cnx);
             cmd.CommandType = CommandType.Text;
-            cmd.Parameters.AddWithValue("@cin", textBox1.Text);
-            cmd.Parameters.AddWithValue("@nom", textBox2.Text);
-            cmd.Parameters.AddWithValue("@prenom", textBox3.Text);
+            cmd.Parameters.AddWithValue("@First_name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Last_name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@cin", textBox3.Text);
             cmd.Parameters.AddWithValue("@age", textBox4.Text);
-            cmd.Parameters.AddWithValue("@location", textBox5.Text);
-            if (radioButton2.Checked)
-                cmd.Parameters.AddWithValue("@AmaladieChro", "oui");
-            else if (radioButton1.Checked)
-                cmd.Parameters.AddWithValue("@AmaladieChro", "non");
-
-            else if (radioButton3.Checked)
-                cmd.Parameters.AddWithValue("@AsympGrave", "oui");
-            else if (radioButton4.Checked)
-                cmd.Parameters.AddWithValue("@AsympGrave", "non");
+            cmd.Parameters.AddWithValue("@adresse", textBox5.Text);
+             cmd.Parameters.AddWithValue("@pid", textBox6.Text);
+          
 
             cnx.Open();
             cmd.ExecuteNonQuery();
@@ -72,7 +65,7 @@ namespace Corona
 
             MessageBox.Show("New Citoyen is saved successefully", "Seved", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            GetCitoyenRecord();
+            GetPatientRecord();
 
         }
 
